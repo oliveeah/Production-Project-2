1b871ae5dbfac1e61e63ea6258828170517cfb5f
oliveeah <olliebennettx@gmail.com>
2026-02-01 22:54:29 +0000

    working on build, it can build but level is not there rn

--- ProductionProjCurr/Intermediate/Build/Win64/UnrealEditor/Inc/ProductionProjCurr/UHT/ProductionProjCurr.init.gen.cpp
+++ ProductionProjCurr/Intermediate/Build/Win64/UnrealEditor/Inc/ProductionProjCurr/UHT/ProductionProjCurr.init.gen.cpp
@@ -23,13 +23,13 @@ void EmptyLinkFunctionForGeneratedCodeProductionProjCurr_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x376AD39D,
-				0xD618E8C6,
+				0x7C070AB7,
+				0xF1F99824,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ProductionProjCurr.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_ProductionProjCurr.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ProductionProjCurr(Z_Construct_UPackage__Script_ProductionProjCurr, TEXT("/Script/ProductionProjCurr"), Z_Registration_Info_UPackage__Script_ProductionProjCurr, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x376AD39D, 0xD618E8C6));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ProductionProjCurr(Z_Construct_UPackage__Script_ProductionProjCurr, TEXT("/Script/ProductionProjCurr"), Z_Registration_Info_UPackage__Script_ProductionProjCurr, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x7C070AB7, 0xF1F99824));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
